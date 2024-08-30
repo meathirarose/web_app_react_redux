775b4dfa8826f1c97a11bb88c2e4c01dc748525b
meathirarose <athirarosejohn@gmail.com>
2024-08-30 22:40:04 +0530

    created user update api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user-routes.js';
 import authRoutes from './routes/auth-routes.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO_LINK)
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log(`Server listening on port ${3000}`);
