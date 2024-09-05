afe1fa600617463dc543479dbd42263a0420518a
meathirarose <athirarosejohn@gmail.com>
2024-09-05 11:01:14 +0530

    admin routes created

--- user/src/components/Header.jsx
+++ user/src/components/Header.jsx
@@ -10,16 +10,16 @@ const Header = () => {
   const isAdminPage = location.pathname.startsWith("/admin");
 
   return (
-    <div
-      className={`${
-        isAdminPage ? "text-white" : " text-black"
-      } shadow-2xl`}
-    >
+    <div className={`${isAdminPage ? "text-white" : " text-black"} shadow-2xl`}>
       <div className="flex justify-between m-auto items-center p-7 max-w-6xl">
         <img src={LOGO_LINK} alt="logo-image" className="w-16 cursor-pointer" />
         <ul className="flex gap-4">
           <li className="px-2 mx-4">
-            <Link to="/">Home</Link>
+            {isAdminPage ? (
+              <Link to="/admin">Home</Link>
+            ) : (
+              <Link to="/">Home</Link>
+            )}
           </li>
           <li className="px-2 mx-4">
             {currentUser ? (
@@ -30,6 +30,8 @@ const Header = () => {
                   className="h-7 w-7 rounded-full object-cover"
                 />
               </Link>
+            ) : isAdminPage ? (
+              <Link to="/admin/sign-in">Sign In</Link>
             ) : (
               <Link to="/sign-in">Sign In</Link>
             )}
